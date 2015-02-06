6d05d4f490877431e6efbdab4802d1718bdc95aa
ympbyc <ympbyc@gmail.com>
2015-02-06 17:39:19 +0900

    allow multiple transition_watchers per key

--- kakahiaka.js
+++ kakahiaka.js
@@ -12,13 +12,30 @@ var kakahiaka = (function () {
         return app;
     }
 
-    function get_watcher (app, key) {
-        return app._watchers[key];
+    function get_watchers (app, key) {
+        return app._watchers[key] || [];
     }
 
-    function add_watch (app, key, f) {
-        app._watchers[key] = f;
+    function add_watch (app, key, f, immidiate) {
+        if (_.has(app._watchers, key))
+            app._watchers[key].push(f);
+        else {
+            app._watchers[key] = [f];
+            if (immidiate && _.has(app._state, key))
+                setTimeout(function () { f(deref(app), {}); }, 0);
+        }
         return app;
+    }
+
+
+    function commit (app, diff, old_s, new_s) {
+        _.each(diff, function (__, key_changed) {
+            _.each(get_watchers(app, key_changed), function (watcher) {
+                setTimeout(function () {
+                    watcher(new_s, old_s);
+                }, 0);
+            });
+        });
     }
 
 
@@ -28,9 +45,14 @@ var kakahiaka = (function () {
      * app :: state * (state -> IO ()) * (() -> state) -> App
      */
     function app (x, persist, recover) {
-        return { _state: recover ? _.conj(x, recover()) : x
-               , _watchers: {}
-               , _persist: persist || function (st) {} };
+        var st = recover ? _.conj(x, recover()) : x;
+        var a =  { _state: st
+                   , _watchers: {}
+                   , _persist: persist || function (st) {} };
+        setTimeout(function () {
+            commit(a, st, {}, st);
+        }, 0);
+        return a;
     }
 
     function deref (app) {
@@ -43,23 +65,21 @@ var kakahiaka = (function () {
      */
     function deftransition (f) {
         return _.optarg(function (app, args) {
-            var old_s = deref(app);
+            var old_s = _.clone(deref(app));
             var diff  = _.apply(_.partial(f, old_s), args);
             if ( ! diff) return undefined;
             var new_s = _.merge(old_s, diff);
-            _.each(diff, function (__, key_changed) {
-                var watcher = get_watcher(app, key_changed);
-                if (watcher)
-                    setTimeout(function () {
-                        watcher(new_s, old_s);
-                    }, 0);
-            });
-            reset_BANG_(app, new_s);
+            commit(app, diff, old_s, new_s);
             app._persist(new_s);
+            reset_BANG_(app, _.omit(new_s, new_s.__meta_keys || []));
             return undefined;
         });
     }
 
+
+    function meta (diff, info) {
+        return _.assoc(_.merge(diff, info), "__meta_keys", _.conj(_.keys(info), "__meta_keys"));
+    }
 
     /**
      * watch_transition :: App * String * ({new state} * {old state} -> undefined) -> undefined
@@ -80,6 +100,8 @@ var kakahiaka = (function () {
         deref:            deref,
         deftransition:    deftransition,
         watch_transition: watch_transition,
-        simple_update:    simple_update
+        watch:            watch_transition,
+        simple_update:    simple_update,
+        meta:             meta
     };
 }());
